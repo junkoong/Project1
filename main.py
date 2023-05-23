cc9efe010f5cd2c4929f0190cbc5489e03bc0e0e
Junhee Lee <leejunkoong@gmail.com>
2023-05-23 14:40:52 +0900

    Commit1 계산기 프로젝트를 시작함. 더하기 함수를 완성함. 완전 어려웠음.

---
+++ main.py
@@ -0,0 +1,12 @@
+## 함수 선언부 (메서드)
+def add_func(n1,n2) :
+    result = n1 + n2
+    return result
+
+## 전역 변수부 (인스턴스 변수)
+num1, num2, res = 100, 200, 0
+
+
+## 메인 코드부 (main() 메서드)
+res = add_func(num1, num2)
+print(num1, '+', num2, '=', res)
