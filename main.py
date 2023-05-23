9ef40dd061d3588daa261dbe5cce6d80d1e9d34c
Junhee Lee <leejunkoong@gmail.com>
2023-05-23 17:28:37 +0900

    Commit3 복구

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def add_func(n1,n2) :
 def sub_func(n1,n2) :
     return n1 - n2
 
+def mul_func(n1,n2) :
+    return n1 * n2
+
+def div_func(n1,n2) :
+    return n1 / n2
+
 ## 전역 변수부 (인스턴스 변수)
 num1, num2, res = 100, 200, 0
 
@@ -17,3 +23,9 @@ print(num1, '+', num2, '=', res)
 
 res = sub_func(num1, num2)
 print(num1, '-', num2, '=', res)
+
+res = mul_func(num1, num2)
+print(num1, '*', num2, '=', res)
+
+res = div_func(num1, num2)
+print(num1, '/', num2, '=', res)
