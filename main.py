e521e163dfd3b99fe2a1303eebe21dafeee80ac9
Junhee Lee <leejunkoong@gmail.com>
2023-05-23 17:34:01 +0900

    Commit4 제곱함수 추가

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def mul_func(n1,n2) :
 def div_func(n1,n2) :
     return n1 / n2
 
+def fac_func(n1,n2) :
+    return n1 ** n2
+
 ## 전역 변수부 (인스턴스 변수)
 num1, num2, res = 100, 200, 0
 
@@ -29,3 +32,6 @@ print(num1, '*', num2, '=', res)
 
 res = div_func(num1, num2)
 print(num1, '/', num2, '=', res)
+
+res = fac_func(num1, num2)
+print(num1, '**', num2, '=', res)
